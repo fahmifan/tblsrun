77e76360a17737572454db65d86948a054b8eead
fahmifan <me@fahmifan.id>
2023-02-06 19:06:34 +0700

    feat: implement configurable docker based tbls

---
+++ config.go
@@ -0,0 +1,44 @@
+package dockertbls
+
+import (
+	"fmt"
+
+	"github.com/joeshaw/envdecode"
+	"github.com/joho/godotenv"
+	"github.com/pkg/errors"
+)
+
+type Database struct {
+	Name     string `env:"DATABASE_NAME"`
+	Schema   string `env:"DATABASE_SCHEMA"`
+	Username string
+	Password string
+	Host     string
+	Port     string
+}
+
+func (db Database) DSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", db.Username, db.Password, db.Host, db.Port, db.Name, db.Schema)
+}
+
+type Config struct {
+	Database     Database
+	MigrationDir string `env:"MIGRATION_DIR"`
+	TblsCfgFile  string `env:"TBLS_CONFIG_FILE,default=.tbls.yml"`
+}
+
+// NewConfig creates an instance of Config.
+// It needs the path of the env file to be used.
+func NewConfig(env string) (Config, error) {
+	err := godotenv.Load(env)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	if err := envdecode.Decode(&config); err != nil {
+		return Config{}, errors.Wrap(err, "[NewConfig] error decoding env")
+	}
+
+	return config, nil
+}
